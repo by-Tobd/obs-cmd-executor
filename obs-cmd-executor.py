3c0bf8aa9cceb863584f1f0e797a741df647c93d
by-Tobd <86555139+by-Tobd@users.noreply.github.com>
2022-04-23 15:47:38 +0200

    Added start on stream

--- obs-cmd-executor.py
+++ obs-cmd-executor.py
@@ -10,7 +10,7 @@ commands = None
 
 def script_description():
 	return """Enables execution of shell commands from obs and autostart them.
-Autostart happens on obs startup and script reload.
+Autostart happens on script load (startup or reload) or on stream start.
 
 e.g.: run Python script:
     \"python C:\path\\to\script.py\"
@@ -21,8 +21,8 @@ e.g.: start new cmd and execute command there:
 by Tobd"""
 
 
+
 def script_update(settings):
-    print(settings)
     global amount_of_commands
     global commands
 
@@ -32,7 +32,8 @@ def script_update(settings):
     for i in range(1, amount_of_commands+1):
         cmd = dict()
         cmd["cmd_list"] = obs.obs_data_get_string(settings, f"cmd_{i}").split()
-        cmd["autostart"] = obs.obs_data_get_bool(settings, f"cmd_autostart_{i}")
+        cmd["start_load"] = obs.obs_data_get_bool(settings, f"cmd_autostart_load_{i}")
+        cmd["start_stream"] = obs.obs_data_get_bool(settings, f"cmd_autostart_stream_{i}")
         
         commands.append(cmd)
 
@@ -42,17 +43,26 @@ def start_process(cmd_list:list):
 
     proc = subprocess.Popen(cmd_list, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
-
-
 def start_process_btn(props, prop):
     index = int(obs.obs_property_name(prop).split("_")[-1]) -1
     start_process(commands[index]["cmd_list"])
 
-def script_load(e):
-    script_update(e)
+def script_load(settings):
+    script_update(settings)
+    obs.obs_frontend_add_event_callback(on_event)
     for cmd in commands:
-        if cmd["autostart"]:
+        if cmd["start_load"]:
             start_process(cmd["cmd_list"])
+
+def on_event(event):
+    if event == obs.OBS_FRONTEND_EVENT_STREAMING_STARTED:
+        streaming_started()
+
+def streaming_started():
+    for cmd in commands:
+        if cmd["start_stream"]:
+            start_process(cmd["cmd_list"])
+
 
 def script_properties():
     props = obs.obs_properties_create()
@@ -61,7 +71,8 @@ def script_properties():
 
     for i in range(1, amount_of_commands+1):
         obs.obs_properties_add_text(props, f"cmd_{i}", f"Command {i}", obs.OBS_TEXT_DEFAULT)
-        obs.obs_properties_add_bool(props, f"cmd_autostart_{i}", f"Command {i}: Autostart")
+        obs.obs_properties_add_bool(props, f"cmd_autostart_load_{i}", f"Command {i}: Execute on load")
+        obs.obs_properties_add_bool(props, f"cmd_autostart_stream_{i}", f"Command {i}: Execute on stream start")
 
         obs.obs_properties_add_button(props, f"start_btn_{i}", f"Start Script {i}", start_process_btn)
 
