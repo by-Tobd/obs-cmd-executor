1188db5de13631888e8728e86c0acb755c7ee7ae
by-Tobd <86555139+by-Tobd@users.noreply.github.com>
2022-04-23 15:13:15 +0200

    Add files via upload

---
+++ obs-cmd-executor.py
@@ -0,0 +1,68 @@
+
+
+
+
+import obspython as obs
+import subprocess
+
+amount_of_commands = 1
+commands = None
+
+def script_description():
+	return """Enables execution of shell commands from obs and autostart them.
+Autostart happens on obs startup and script reload.
+
+e.g.: run Python script:
+    \"python C:\path\\to\script.py\"
+
+e.g.: start new cmd and execute command there:
+    \"cmd /k <cmd to execute>\"
+
+by Tobd"""
+
+
+def script_update(settings):
+    print(settings)
+    global amount_of_commands
+    global commands
+
+    amount_of_commands   = obs.obs_data_get_int(settings, "amount_of_commands")
+    commands         = list()
+
+    for i in range(1, amount_of_commands+1):
+        cmd = dict()
+        cmd["cmd_list"] = obs.obs_data_get_string(settings, f"cmd_{i}").split()
+        cmd["autostart"] = obs.obs_data_get_bool(settings, f"cmd_autostart_{i}")
+        
+        commands.append(cmd)
+
+    
+def start_process(cmd_list:list):
+    print(f"Starting \"{' '.join(cmd_list)}\"")
+
+    proc = subprocess.Popen(cmd_list, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+
+
+def start_process_btn(props, prop):
+    index = int(obs.obs_property_name(prop).split("_")[-1]) -1
+    start_process(commands[index]["cmd_list"])
+
+def script_load(e):
+    script_update(e)
+    for cmd in commands:
+        if cmd["autostart"]:
+            start_process(cmd["cmd_list"])
+
+def script_properties():
+    props = obs.obs_properties_create()
+
+    obs.obs_properties_add_int(props, "amount_of_commands", "Amount of Scripts", 1, 10, 1)
+
+    for i in range(1, amount_of_commands+1):
+        obs.obs_properties_add_text(props, f"cmd_{i}", f"Command {i}", obs.OBS_TEXT_DEFAULT)
+        obs.obs_properties_add_bool(props, f"cmd_autostart_{i}", f"Command {i}: Autostart")
+
+        obs.obs_properties_add_button(props, f"start_btn_{i}", f"Start Script {i}", start_process_btn)
+
+    return props
